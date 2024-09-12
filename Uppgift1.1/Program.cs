ef98e7b4a393e72394d9baf96efa07b20387664b
maol07001 <maol07001@E-PW05CGR4>
2024-09-12 14:07:32 +0200

    Add project files.

---
+++ Uppgift1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace uppgift_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");//console.WriteLine("") gör så att man kan skriva skaer.
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\.");// när man ska skriva citationstecken och snedstreck i Console.WriteLine("")behöver man ha ett \ motsat snedsteck.
+        }
+    }
+}
